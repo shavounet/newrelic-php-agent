26ae843f4c7c8df68cb51aa5dcebe299e0e6c141
Brian Duranleau <bduranleau@newrelic.com>
2026-04-16 16:20:32 -0500

    feat: initial implementation of txn_global copy functionality

--- agent/php_newrelic.h
+++ agent/php_newrelic.h
@@ -130,6 +130,21 @@ typedef struct _nrcallbackfn_t {
   bool is_set;
 } nrcallbackfn_t;
 
+typedef struct _txn_globals_t {
+  int execute_count; /* How many times nr_php_execute_enabled was called */
+  int generating_explain_plan; /* Are we currently working on an explain plan?
+                                */
+  nr_hashmap_t* guzzle_objs; /* Guzzle request object storage: requests that are
+                                currently in progress are stored here */
+  nr_mysqli_metadata_t* mysqli_links; /* MySQLi link metadata storage */
+  nr_hashmap_t* mysqli_queries;       /* MySQLi query metadata storage */
+  nr_hashmap_t* pdo_link_options;     /* PDO link option storage */
+  int curl_ignore_setopt; /* Non-zero to disable curl_setopt instrumentation */
+  nr_hashmap_t* curl_metadata;       /* curl metadata storage */
+  nr_hashmap_t* curl_multi_metadata; /* curl multi metadata storage */
+  nr_hashmap_t* prepared_statements; /* Prepared statement storage */
+} txn_globals_t;
+
 /*
  * Per-request globals. This is designed for thread safety.
  * These are the globals that are accessible to each request, of which
@@ -617,20 +632,8 @@ nr_segment_t* fiber_parent_segment;
  * information related to the active transaction and, contrary to the globals
  * above, have to be reset for each transaction started during a request.
  */
-struct {
-  int execute_count; /* How many times nr_php_execute_enabled was called */
-  int generating_explain_plan; /* Are we currently working on an explain plan?
-                                */
-  nr_hashmap_t* guzzle_objs; /* Guzzle request object storage: requests that are
-                                currently in progress are stored here */
-  nr_mysqli_metadata_t* mysqli_links; /* MySQLi link metadata storage */
-  nr_hashmap_t* mysqli_queries;       /* MySQLi query metadata storage */
-  nr_hashmap_t* pdo_link_options;     /* PDO link option storage */
-  int curl_ignore_setopt; /* Non-zero to disable curl_setopt instrumentation */
-  nr_hashmap_t* curl_metadata;       /* curl metadata storage */
-  nr_hashmap_t* curl_multi_metadata; /* curl multi metadata storage */
-  nr_hashmap_t* prepared_statements; /* Prepared statement storage */
-} txn_globals;
+
+txn_globals_t txn_globals;
 
 ZEND_END_MODULE_GLOBALS(newrelic)
 
